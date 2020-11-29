9a5d59f2fd4a23aa914eddb744c50c44eb5e361e
92SooJong <hsz3855@gmail.com>
2020-11-29 11:05:31 +0900

    65페이지까지 실습.

--- taco-cloud/src/main/java/soojong/tacocloud/tacos/Order.java
+++ taco-cloud/src/main/java/soojong/tacocloud/tacos/Order.java
@@ -1,16 +1,37 @@
 package soojong.tacocloud.tacos;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import lombok.Data;
 
 @Data
 public class Order {
 	
+	@NotBlank(message="Name is required")
 	private String deliveryName;
+	
+	@NotBlank(message="Street is required")
 	private String deliveryStreet;
+	
+	@NotBlank(message="City is required")
 	private String deliveryCity;
+	
+	@NotBlank(message="State is required")
 	private String deliveryState;
+	
+	@NotBlank(message="Zip is required")
 	private String deliveryZip;
+	
+	@CreditCardNumber(message = "유효한 카드번호가 아닙니다")
 	private String ccNumber;
+	
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",message="Must be formatted MM/YY")
 	private String ccExpiration;
+	
+	@Digits(integer=3, fraction=0, message="Invalid CCV")
 	private String ccCVV;
 }
--- taco-cloud/src/main/java/soojong/tacocloud/tacos/Taco.java
+++ taco-cloud/src/main/java/soojong/tacocloud/tacos/Taco.java
@@ -1,12 +1,19 @@
 package soojong.tacocloud.tacos;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
+
 
 @Data
 public class Taco {
 	
+	@NotNull
+	@Size(min=5,message="Name must be at least 5 characters long")
 	private String name;
+	
+	@Size(min=1, message="적어도 1개의 재료는 선택해야합니다.")
 	private List<String> ingredients;
 }
