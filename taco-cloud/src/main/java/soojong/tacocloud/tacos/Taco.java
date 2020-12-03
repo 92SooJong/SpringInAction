5e576332e44cd959c5bff064f9c05e6bb421623b
92SooJong <hsz3855@gmail.com>
2020-12-03 22:57:53 +0900

    108페이지까지 완료

--- taco-cloud/src/main/java/soojong/tacocloud/tacos/Taco.java
+++ taco-cloud/src/main/java/soojong/tacocloud/tacos/Taco.java
@@ -2,6 +2,13 @@ package soojong.tacocloud.tacos;
 
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,8 +16,11 @@ import lombok.Data;
 
 
 @Data
+@Entity
 public class Taco {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private Date createdAt;
 	
@@ -18,6 +28,13 @@ public class Taco {
 	@Size(min=5,message="Name must be at least 5 characters long")
 	private String name;
 	
+	@ManyToMany(targetEntity = Ingredient.class)
 	@Size(min=1, message="적어도 1개의 재료는 선택해야합니다.")
 	private List<Ingredient> ingredients;
+	
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
+	
 }
