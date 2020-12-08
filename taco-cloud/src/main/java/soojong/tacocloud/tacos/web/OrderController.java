2e68bfc3714271288eec0c5dfe2a68fd1f059889
92SooJong <hsz3855@gmail.com>
2020-12-09 08:53:09 +0900

    162페이지까지 실습완료.

--- taco-cloud/src/main/java/soojong/tacocloud/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/soojong/tacocloud/tacos/web/OrderController.java
@@ -2,10 +2,11 @@ package soojong.tacocloud.tacos.web;
 
 import javax.validation.Valid;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import soojong.tacocloud.tacos.Order;
+import soojong.tacocloud.tacos.User;
 import soojong.tacocloud.tacos.data.OrderRepository;
 
 @Slf4j
@@ -28,17 +30,35 @@ public class OrderController {
 	}
 	
 	@GetMapping("/current")
-	public String orderForm() {
+	public String orderForm(@AuthenticationPrincipal User user , @ModelAttribute Order order ) {
+		
+		if (order.getDeliveryName() == null) {
+			  order.setDeliveryName(user.getFullname());
+		}
+		if (order.getDeliveryStreet() == null) {
+			  order.setDeliveryStreet(user.getStreet());
+		}
+		if (order.getDeliveryCity() == null) {
+			  order.setDeliveryCity(user.getCity());
+		}
+		if (order.getDeliveryState() == null) {
+			  order.setDeliveryState(user.getState());
+		}
+		if (order.getDeliveryZip() == null) {
+			  order.setDeliveryZip(user.getZip());
+		}
 		
 		return "orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder(@Valid Order order,Errors errors, SessionStatus sessionStatus) {
+	public String processOrder(@Valid Order order,Errors errors, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
 		
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
+		
+		order.setUser(user);
 		
 		orderRepo.save(order);
 		sessionStatus.setComplete();
