e7f9b2e39f2d5196fe47f2aa8d6e2b28cace6102
92SooJong <hsz3855@gmail.com>
2020-12-14 23:00:00 +0900

    연습폴더 업데이트.

--- practice/newLecture1/src/newLecture1/Program.java
+++ practice/newLecture1/src/newLecture1/Program.java
@@ -5,7 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		// IOC : 작은 부품을 먼저 만들어 놓고 큰 부붐에 끼운다.
-		// IOC가 없는 경우는 큰 부붐을 먼저 만들고 작은 부품을 조립한다.
+		// IOC가 없는 경우는 큰 부품을 먼저 만들고 작은 부품을 조립한다.
+		
 		
 		Exam exam = new NewlecExam();
 		// InlineExamConsole이 Exam을 조립하고 있다. ( DI )
